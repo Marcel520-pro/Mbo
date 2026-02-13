098ccfa30731f6fcd15e69c04b1a4d058357f011
Marcel520-pro <jerryfeudjio@gmail.com>
2026-02-13 11:44:40 +0100

    ajout de l'entite Utilisateur

--- src/main/java/org/logonedigital/mboa_care/entity/Utilisateur.java
+++ src/main/java/org/logonedigital/mboa_care/entity/Utilisateur.java
@@ -1,4 +1,33 @@
 package org.logonedigital.mboa_care.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Utilisateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(length = 36, nullable = false, unique = true, updatable = false)
+    private String idUtilisateur;
+    private String nomUtilisateur;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false, unique = true)
+    private String password;
+    private String telephone;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
 }
