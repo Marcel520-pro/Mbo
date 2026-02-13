40a37e7ed2d81cde08ff34a2e25b8e274271be47
Marcel520-pro <jerryfeudjio@gmail.com>
2026-02-13 11:26:50 +0100

    Intial project commit

---
+++ src/main/java/org/logonedigital/mboa_care/MboaCareApplication.java
@@ -0,0 +1,13 @@
+package org.logonedigital.mboa_care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MboaCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MboaCareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/logonedigital/mboa_care/MboaCareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.logonedigital.mboa_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MboaCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
