8ed4ef9f6b9f27e7e98eba34a8de6c612cd79189
Marcel520-pro <jerryfeudjio@gmail.com>
2026-02-13 12:16:25 +0100

    ajout de la fonctionalite consulterProfil

--- src/main/java/org/logonedigital/mboa_care/controllers/profilControllers.java
+++ src/main/java/org/logonedigital/mboa_care/controllers/profilControllers.java
@@ -2,12 +2,10 @@ package org.logonedigital.mboa_care.controllers;
 
 import jakarta.validation.Valid;
 import org.logonedigital.mboa_care.dto.UtilisateurReqDto;
+import org.logonedigital.mboa_care.dto.UtilisateurResDto;
 import org.logonedigital.mboa_care.service.ProfilService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/profils")
@@ -22,5 +20,10 @@ public class profilControllers {
     public ResponseEntity<String> creerProfil(@RequestBody @Valid UtilisateurReqDto utilisateurReqDto) {
         this.profilService.creerProfil(utilisateurReqDto);
         return ResponseEntity.status(200).body("profil successfully created");
+    }
+
+    @GetMapping(path = "/consulter_profil{idUtilisateur}")
+    public ResponseEntity<UtilisateurResDto> consulterProfil(@PathVariable("idUtilisateur") String idUtilisateur) {
+        return ResponseEntity.status(200).body(this.profilService.consulterProfil(idUtilisateur));
     }
 }
