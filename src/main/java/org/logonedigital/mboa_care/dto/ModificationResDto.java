e152b7c5346b4d1c45c460ccd347e949e645edab
Marcel520-pro <jerryfeudjio@gmail.com>
2026-02-13 12:24:08 +0100

    ajout du dto modificationResDto

---
+++ src/main/java/org/logonedigital/mboa_care/dto/ModificationResDto.java
@@ -0,0 +1,9 @@
+package org.logonedigital.mboa_care.dto;
+
+public class ModificationResDto {
+    private String nomUtilisateur;
+    private String email;
+    private String telephone;
+    private String ville;
+    private String quartier;
+}
