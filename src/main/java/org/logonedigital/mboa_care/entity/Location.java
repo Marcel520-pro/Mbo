67cf90d933f7880cd920defdf052c20e29384b03
emmanuellebnz <benzoemmanuelle@gmail.com>
2026-02-15 03:28:21 +0100

    creation des dto (Data Transfer Objects (Dto))

--- src/main/java/org/logonedigital/mboa_care/entity/Location.java
+++ src/main/java/org/logonedigital/mboa_care/entity/Location.java
@@ -1,4 +1,21 @@
 package org.logonedigital.mboa_care.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "locations")
 public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String idLocation;
+    private String ville;
+    private String quartier;
 }
--- src/main/java/org/logonedigital/mboa_care/entity/Teleconsultation.java
+++ src/main/java/org/logonedigital/mboa_care/entity/Teleconsultation.java
@@ -1,4 +1,34 @@
 package org.logonedigital.mboa_care.entity;
 
+import ch.qos.logback.core.status.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "consultations")
 public class Teleconsultation {
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private String idConsultation;
+    private LocalDateTime consultationDate;
+    @Column(length = 100, nullable = false)
+    private String motif;
+    @Enumerated(EnumType.STRING)
+    private Statut status;
+
+    @ManyToOne
+    @JoinColumn(name = "idPatient", nullable = false)
+    private Utilisateur patient;
+
+    @ManyToOne
+    @JoinColumn(name = "idMedecin", nullable = false)
+    private Utilisateur medecin;
 }
