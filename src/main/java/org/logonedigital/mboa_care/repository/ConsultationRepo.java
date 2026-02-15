4af4cba9bc2f10e55f036a5877ef2b5b115e6aa7
emmanuellebnz <benzoemmanuelle@gmail.com>
2026-02-15 04:44:58 +0100

    ajout du Dto de location , LocationReqDto

--- src/main/java/org/logonedigital/mboa_care/repository/ConsultationRepo.java
+++ src/main/java/org/logonedigital/mboa_care/repository/ConsultationRepo.java
@@ -1,4 +1,10 @@
 package org.logonedigital.mboa_care.repository;
 
-public interface ConsultationRepo {
+import org.logonedigital.mboa_care.entity.Teleconsultation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConsultationRepo extends JpaRepository<Teleconsultation, String> {
+
 }
