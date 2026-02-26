ec6216df4eadc5969655c5535e570409fdfadafb
emmanuellebnz <benzoemmanuelle@gmail.com>
2026-02-26 16:20:02 +0100

    ajout des attributs de Rendezvous

---
+++ src/main/java/org/logonedigital/mboa_care/entity/CompteRendu.java
@@ -0,0 +1,4 @@
+package org.logonedigital.mboa_care.entity;
+
+public class CompteRendu gi{
+}
--- src/main/java/org/logonedigital/mboa_care/entity/Rendez_vous.java
+++ src/main/java/org/logonedigital/mboa_care/entity/Rendez_vous.java
@@ -1,4 +1,26 @@
 package org.logonedigital.mboa_care.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Table(name="rendez_vous")
+
 public class Rendez_vous {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+
+    private String id;
+    private String motif;
+    private LocalDateTime dateHeure;
+
 }
